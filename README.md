a8ccf5b9dffc4fac5e1624349c369863c95d774e
Sean Bone <info@seanbone.ch>
2022-03-11 16:01:38 +0100

    Updated install script

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Clone the repo and run the installation script:
 
 Note that this may mess with previous installations of vim. Check the install script before running!
 
+Also, don't forget to set the JetBrainsMono patched font as the font for your terminal.
+
 Upon opening vim, use `:PlugInstall` to cause [vim-plug](https://github.com/junegunn/vim-plug) to refresh the plugin installations.
 
 To automatically install the `clangd` language server with the [`coc-clangd`](https://github.com/clangd/coc-clangd) CoC extension, run the command `:CocCommand clangd.install`.
--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ ln -s nvim.appimage vim
 
 # Make a symlink to this repo's init.nvim
 mkdir -p ~/.config/nvim/
-ln -s ~/.config/nvim/init.vim init.vim
+ln -s `realpath init.vim` ~/.config/nvim/init.vim
 
 # Install Node.js, which is required for the CoC completion plugin:
 curl -sL install-node.vercel.app/lts | sudo bash
@@ -21,3 +21,9 @@ curl -fLo /tmp/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/release
 mkdir -p /tmp/JetBrainsMono && unzip -qd /tmp/JetBrainsMono /tmp/JetBrainsMono.zip
 mkdir -p ~/.local/share/fonts/NerdFonts/
 cp '/tmp/JetBrainsMono/JetBrains Mono Regular Nerd Font Complete.ttf' ~/.local/share/fonts/NerdFonts/
+
+# Install vim plugins
+vim --headless -c "PlugInstall" -c "qall"
+
+# Install clangd
+vim --headless -c "CocCommand clangd.install" -c "qall" /tmp/whatever.cpp
